376b0888c5779e8189a341612ca4a5a766792483
Discord.js Arabic Developer <32039975+ShadwX57@users.noreply.github.com>
2018-10-19 17:38:43 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,6 @@ const client = new Discord.Client();
 const fs = require('fs');
 const suck = JSON.parse(fs.readFileSync('./suck.json', 'utf8'));
 const prefix = "r#";
-// By M7MD
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag} !`);
           client.user.setActivity("Rainbow , r#help .",{type: 'WATCHING'});
