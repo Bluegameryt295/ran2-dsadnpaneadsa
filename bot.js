1963305146d8554ea92f3d22072187e2799a2190
Bluegameryt295 <42852697+Bluegameryt295@users.noreply.github.com>
2018-11-27 16:38:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -48,5 +48,30 @@ message.author.send(`**
 r#set 
  - لإنشاء رتبة الرينبو وبدا الرينبو
 - To create the role of the Rainbow & Start The Rainbow
-
+r#inv 
+- لدعوة البوت
+- To Invite the bot
+خطوات لو الرتبة م أشتغلت .!!
+1- ضع رتبة الرينبو فوق الالوان أو الرتب الملونه لو فيه
+2- ضع رتبة البوت فوق رتبة الرينبو
+The steps of the role did not worked .!!
+1- Place the role of the Rainbow above the colors or colored ranks if it
+2- Put the bot role above the role of the Rainbow 
+رآبط البوت - Bot Invite link
+- http://cutt.us/RainbowBott
+**`)
+  }})
+   client.on('message', message => {
+	   if(message.content.startsWith(`r#inv`)){
+		   if(!message.channel.guild) return message.channel.send("This Command is Just For Servers!")
+                 message.react('🌈')
+		   var embed = new Discord.RichEmbed()
+		   .setTitle(">> ClickHere To Add" + `${client.user.username}` + " <<")
+		   .setURL("https://discordapp.com/oauth2/authorize?client_id=" + `${client.user.id}` + "&scope=bot&permissions=2080374975")
+		   .setTimestamp()
+		   .setFooter(`Requested By | ${message.author.username}`)
+		   .setColor("RANDOM")
+		   message.author.send({embed})
+	   }
+   });
 client.login(process.env.BOT_TOKEN)
